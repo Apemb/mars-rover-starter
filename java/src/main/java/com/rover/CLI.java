c37bf8f9f6887df87f52b17f1fad97d2e39c3362
Antoine Boileau <apem.boileau@gmail.com>
2018-06-03 20:54:51 +0200

    Add command line options

--- java/src/main/java/com/rover/CLI.java
+++ java/src/main/java/com/rover/CLI.java
@@ -6,15 +6,56 @@ public class CLI {
 
     private static Options configParameters() {
 
-        Option logfile = Option.builder("file")
-                .required(true)
+        Option positionX = Option.builder("x")
+                .desc("initial X landing rover position")
+                .longOpt("positionX")
                 .hasArg()
-                .longOpt("arg-name")
+                .type(Integer.class)
+                .required(true)
                 .build();
+
+        Option positionY = Option.builder("y")
+                .desc("initial Y landing rover position")
+                .longOpt("positionY")
+                .hasArg()
+                .type(Integer.class)
+                .required(true)
+                .build();
+
+        Option direction = Option.builder("d")
+                .desc("initial landing rover direction")
+                .longOpt("direction")
+                .hasArg()
+                .type(String.class)
+                .required(true)
+                .build();
+
+        Option commands = Option.builder("c")
+                .desc("provide an array of commands for the rover to execute")
+                .longOpt("commands")
+                .hasArgs()
+                .type(String[].class)
+                .required(true)
+                .build();
+
+        /*
+        Rover initialization:
+        --positionX, -x  initial X landing rover position           [required]
+        --positionY, -y  initial Y landing rover position           [required]
+        --direction, -d  initial landing rover direction            [required]
+
+        Rover commands:
+        --commands, -c  provide an array of commands for the rover to execute [array]
+
+        */
+
 
         final Options options = new Options();
 
-        options.addOption(logfile);
+        options.addOption(positionX);
+        options.addOption(positionY);
+        options.addOption(direction);
+        options.addOption(commands);
 
         return options;
     }
@@ -32,14 +73,15 @@ public class CLI {
             line = parser.parse(options, args);
 
             HelpFormatter formatter = new HelpFormatter();
-            formatter.printHelp("rover", options);
+            formatter.printHelp("initialization of rover", options);
 
         } catch (ParseException e) {
 
-            HelpFormatter formatter = new HelpFormatter();
-            formatter.printHelp("rover", options);
+            System.out.println(e.getMessage() + "\n");
 
-            e.printStackTrace();
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("java -jar target/rover-1.0-jar-with-dependencies.jar", options);
+
         }
 
         System.out.println("\n~~~~~ ROVER TERMINATED ~~~~~\n");
